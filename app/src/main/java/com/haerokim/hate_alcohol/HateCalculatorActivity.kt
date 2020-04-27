39b839f7dc37b11369db0afced04d52c13113a74
H43RO <h2is1234@naver.com>
2020-04-27 18:24:10 +0900

    Implementation some functions

--- app/src/main/java/com/haerokim/hate_alcohol/HateCalculatorActivity.kt
+++ app/src/main/java/com/haerokim/hate_alcohol/HateCalculatorActivity.kt
@@ -2,11 +2,26 @@ package com.haerokim.hate_alcohol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_hate_calculator.*
 
 class HateCalculatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hate_calculator)
+        var count = 0
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, count: Int, p2: Boolean) {
+                count_member.setText(count.toString())
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
     }
 }
